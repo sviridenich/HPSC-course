75bb32500cad9a74e9477f213dd253b9bf9e9976
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-12 21:08:48 +0300

    generalized poly function is written, although not extensively covered by test

--- homework2/hw2b.py
+++ homework2/hw2b.py
@@ -60,7 +60,25 @@ def cubic_interp(xi,yi):
 
     return c
 
+def poly_interp(xi,yi):
+    '''
+    function that generalize the above functions to accept arrays xi and yi of any length n
+    '''
 
+    # check inputs and print error message if not valid:
+
+    error_message = "xi and yi should have type numpy.ndarray"
+    assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+    error_message = "xi and yi should have the same length"
+    assert len(xi) == len(yi), error_message
+    
+    A = np.vstack([xi**p for p in range( len(xi) )]).T  
+    b = yi
+    c = solve(A,b)
+
+    return c
+           
 def quad_plot(xi,yi):
     '''
     It takes two numpy arrays xi and yi of length 3, 
@@ -130,6 +148,43 @@ def cubic_plot(xi,yi):
     plt.ylim(yi.min() -1 ,yi.max() + 1)         # set limits in y for plot
     plt.title("Data points and interpolating polynomial") 
     plt.savefig('cubic.png')   # save figure as .png file
+    
+    
+def poly_plot(xi, yi):
+    '''
+    It takes two numpy arrays xi and yi of the same arbitray length, 
+    calls ploy_interp to compute c, 
+    and then plots both the interpolating polynomial and the data points, 
+    and saves the resulting figure as cubic.png. 
+    '''    
+
+    error_message = "xi and yi should have type numpy.ndarray"
+    assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+    error_message = "xi and yi should have the same length"
+    assert len(xi) == len(yi), error_message
+    
+    # compute coefficients
+  
+    c = poly_interp(xi,yi)
+
+    # then plots both the interpolating polynomial and the data points
+    
+    x = np.linspace( xi.min() -1 ,xi.max() + 1,1001)   # points to evaluate polynomial
+    y = c[0]
+    for i in range(1,len(xi),1):      
+        y += c[i]*x**i 
+
+    plt.figure(1)       # open plot figure window
+    plt.clf()           # clear figure
+    plt.plot(x,y,'b-')  # connect points with a blue line
+
+    # Add data points  (polynomial should go through these points!)
+    plt.plot(xi,yi,'ro')   # plot as red circles
+    plt.ylim(yi.min() -1 ,yi.max() + 1)         # set limits in y for plot
+    plt.title("Data points and interpolating polynomial") 
+    plt.savefig('poly.png')   # save figure as .png file
+    
 
 def test_quad1():
     """
@@ -173,6 +228,19 @@ def test_cubic1():
     assert np.allclose(c, c_true), \
         "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
 
+def test_poly1():
+    """
+    Test code, no return value or exception if test runs properly.
+    """
+    xi = np.array([-1.,  0.,  1., 2.])
+    yi = np.array([ 0.,  1.,  2., 9.])
+    c = poly_interp(xi,yi)
+    c_true = np.array([ 1.,  0.,  0., 1. ])
+    print "c =      ", c
+    print "c_true = ", c_true
+    # test that all elements have small error:
+    assert np.allclose(c, c_true), \
+        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
 
 if __name__=="__main__":
     # "main program"
@@ -185,3 +253,4 @@ if __name__=="__main__":
     test_quad1()
     test_quad2()
     test_cubic1()
+    test_poly1()
