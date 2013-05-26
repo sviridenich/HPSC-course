e665aa91c87f157587dd9b827b97732e4940f836
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-26 16:19:16 +0300

    realiasition of newton's method for single function is shipped

--- homework3/newton.py
+++ homework3/newton.py
@@ -7,8 +7,25 @@ debug, an optional argument with default False.
 The function should return a tuple consisting of the final iterate (the approximation to the root determined) and the number of iterations taken.
 '''
 
-def solve():
-    return 1
+def solve(fvals,x0, debug = False):
+    maxiter = 20
+    tol = 1e-14
+    
+    if (debug):
+        print "Initial guess is %22.15e" % x0
+        
+    x = x0
+    for i in range(maxiter):
+        
+        if ((debug) & (i > 0 )):
+            print "after %i iterations, x = %22.15e" %(i, x)
+            
+        fx,fxprime = fvals(x)
+        if abs(fx) < tol :
+            break
+        x = x - fx/fxprime
+        
+    return (x, i)
 
 def fvals_sqrt(x):
     """
@@ -18,7 +35,7 @@ def fvals_sqrt(x):
     fp = 2.*x
     return f, fp
 
-def test1(debug_solve=False):
+def test1(debug_solve=True):
     """
     Test Newton iteration for the square root with different initial
     conditions.
