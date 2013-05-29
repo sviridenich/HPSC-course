684b87acd62f962043f9af1d9797f92b7d55383b
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-29 20:49:01 +0300

    starter code for next task

---
+++ homework3/intersection.py
@@ -0,0 +1,7 @@
+'''
+The purpose of the program is to find intersection of two functions g1(x) and g2(x) by the means of the previously written newton.py.
+Furthermore once task is solved, we need to provide some visulation.
+'''
+
+from newton import solve
+
